66af5293a26eff204ca6bd3d7ce710b7e7ab3fe2
g7h0b <g7h0b@ugrad.cs.ubc.ca>
2018-03-24 13:37:32 -0700

    Starter file

--- UIbasic.java
+++ UIbasic.java
@@ -9,7 +9,6 @@ public class UIbasic extends JFrame implements ActionListener {
     private JPasswordField password;
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         new UIbasic();
     }
 
