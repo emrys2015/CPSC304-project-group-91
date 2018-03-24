5477d033e38e9d75e77c9bf51437454e26454373
g7h0b <g7h0b@ugrad.cs.ubc.ca>
2018-03-24 13:34:47 -0700

    Starter file

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 
+
 public class Main {
+    private UIbasic UIlogin;
 
     public static void main(String[] args) {
 	// write your code here
+        new Main();
+    }
+
+    public Main(){
+        UIlogin = new UIbasic();
     }
 }
