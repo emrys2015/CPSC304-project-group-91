4bb31079e44226fdb7104edfd5a2ea1560298836
emrys2015 <33079067+emrys2015@users.noreply.github.com>
2018-03-28 17:36:04 -0700

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -15,14 +15,86 @@ public class Manager {
         this.passWord = passWord;
         this.con = con;
     }
-
-    public void addDriver(String login, String employeeName, String ssn){
+public void addEmployee( String E_pass, String E_SSN, String E_ADD, String E_Name, String E_PHNO, String E_POS) {
         //todo
+        try {
 
+            PreparedStatement preparedStatement = con.prepareStatement("INSERT INTO EMPLOYEE VALUES (?,?,?,?,?,?)");
+            preparedStatement.setInt(1, Integer.parseInt(E_pass));
+            preparedStatement.setInt(2, Integer.parseInt(E_SSN));
+            preparedStatement.setString(3, E_ADD);
+            preparedStatement.setString(4, E_Name);
+            preparedStatement.setInt(5, Integer.parseInt(E_PHNO));
+            preparedStatement.setString(6, E_POS);
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+
+        } catch (SQLException ex) {
+            System.out.println("Message: " + ex.getMessage());
+            try {
+                // undo the insert
+                con.rollback();
+            } catch (SQLException ex2) {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
+        }
     }
 
-    public void deleteDriver(String recieverAddress, String recieverPhoneNo, int Weight, String DeliverType){
+    public void addDriver(String DR_SSN, String DR_DRLN, String DR_OFF_NO){
+            //todo
+        try{
+                        PreparedStatement preparedStatement= con.prepareStatement ("INSERT INTO DRIVER VALUES (?,?,?)");
+                        preparedStatement.setInt(1, Integer.parseInt(DR_SSN));
+                        preparedStatement.setInt(2, Integer.parseInt(DR_DRLN));
+                        preparedStatement.setInt(3, Integer.parseInt(DR_OFF_NO));
+
+                        preparedStatement.executeUpdate();
+                        preparedStatement.close();
+                    }
+                catch(SQLException ex)
+                {
+                            System.out.println("Message: " + ex.getMessage());
+                   try
+                    {
+                                // undo the insert
+                                       con.rollback();
+                    }
+                    catch (SQLException ex2)
+                    {
+                                System.out.println("Message: " + ex2.getMessage());
+                        System.exit(-1);
+                    }
+                }
+
+
+        }
+    public void deleteDriver(String DR_SSN, String DR_DRLN, String DR_OFF_NO){
         //todo
+
+        try{
+            PreparedStatement preparedStatement= con.prepareStatement ("DELETE FROM DRIVER WHERE DRI_SSN = ?, DRI_DLNO = ?, DR_OFF_NO = ?");
+            preparedStatement.setInt(1, Integer.parseInt(DR_SSN));
+            preparedStatement.setInt(2, Integer.parseInt(DR_DRLN));
+            preparedStatement.setInt(3, Integer.parseInt(DR_OFF_NO));
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        }
+        catch(SQLException ex)
+        {
+            System.out.println("Message: " + ex.getMessage());
+            try
+            {
+                // undo the insert
+                con.rollback();
+            }
+            catch (SQLException ex2)
+            {
+                System.out.println("Message: " + ex2.getMessage());
+                System.exit(-1);
+            }
+        }
 
     }
 
